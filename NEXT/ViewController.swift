1d2130eeb17c67808abc02b8e0de46650dac063a
Ridhwan Chowdhury <ridhwan.c@gmail.com>
2019-07-09 10:40:02 -0400

    Initial Commit

---
+++ NEXT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NEXT
+//
+//  Created by Ridhwan Ahabab Chowdhury on 2019-07-09.
+//  Copyright © 2019 Ridhwan  Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
